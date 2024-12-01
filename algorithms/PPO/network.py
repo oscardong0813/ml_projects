a1a2a3426475ed03f396dd581184803087cb751f
Lu Dong <odong0813@gmail.com>
2024-12-01 12:17:21 -0500

    implemented critic network

--- algorithms/PPO/network.py
+++ algorithms/PPO/network.py
@@ -3,6 +3,55 @@ from torch import nn
 import torch.nn.functional as F
 import numpy as np
 
-class FeedForwardNN(nn.Module):
-    def __init__(self):
-        super(FeedForwardNN, self).__init__()
+HIDDEN_SIZES = (64, 64)
+ACTIVATION = nn.Tanh
+class CriticNet(nn.Module):
+    '''
+    critic network
+    input:
+    state dimensions,
+    a list of neurons in hidden layer
+
+    '''
+    def __init__(self, state_dim, hidden_sizes=HIDDEN_SIZES):
+        super().__init__()
+        self.layers = nn.ModuleList()
+        self.activation = ACTIVATION()
+        prev = state_dim
+
+        for h in hidden_sizes:
+            layer = nn.Linear(prev, h)
+            self.layers.append(layer)
+            prev = h
+            # self.layers.append(nn.Tanh())
+        self.final = nn.Linear(prev, 1)
+
+    def forward(self, state):
+        '''
+        :param state: a pytorch tensor
+        :return: an estimate of the expected return form the current state.
+        '''
+        activation = state
+        for layer in self.layers:
+            activation = F.tanh(layer(activation))
+        output = self.final(activation)
+        return output
+
+class ActorNet(nn.Module):
+    '''
+    actor network
+    input:
+    state dim
+    action dim
+    a list of neurons in hidden layer
+    '''
+    def __init__(self, state_dim, action_dim, hid_sizes = HIDDEN_SIZES):
+        pass
+
+
+if __name__ == '__main__':
+
+    critic = CriticNet(10)
+    print(critic.layers)
+    state = torch.tensor([1.,2.,3.,4.,5.,6.,7.,8.,9.,10.])
+    print(critic.forward(state))
